dbd611c78ffe70739164b086ffd5358cbea1fa0d
Tejal002 <121813109+Tejal002@users.noreply.github.com>
2026-02-18 21:47:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let element=document.querySelector("level");
+let count=0;
+	while(element){
+		count++;
+		element=element.parentElement;
+	}
+
+document.alert(count);
