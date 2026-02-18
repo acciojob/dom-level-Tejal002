37d8e24ee560495eedd9d6ca07d3158d1e165fd6
Tejal002 <121813109+Tejal002@users.noreply.github.com>
2026-02-18 21:51:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 //your JS code here. If required.
-let element=document.querySelector("level");
+let element=document.querySelector("#level");
 let count=0;
 	while(element){
 		count++;
 		element=element.parentElement;
 	}
-
-document.alert(count);
+alert("The level of the element is: " + count);
